04ed8e06ee20b4d53601f09a60857d02c2805eff
张志豪 <izhangzhihao@hotmail.com>
2016-08-26 10:40:20 +0800

    整理代码，增加注释

--- src/test/java/com/github/izhangzhihao/SpringMVCSeedProject/Test/ControllerTest/LogControllerTest.java
+++ src/test/java/com/github/izhangzhihao/SpringMVCSeedProject/Test/ControllerTest/LogControllerTest.java
@@ -68,10 +68,45 @@ public class LogControllerTest extends BaseTest {
      */
     @Test
     public void getLogByPageTest() throws Exception {
-        //自动将json转为实体，避免了手动转换
-        PageResults pageResults = restTemplate
+        //自动将json转为实体，避免了手动转换，缺点是不可以测试http status
+        /*PageResults pageResults = restTemplate
                 .getForObject("/Log/getLogByPage/pageNumber/" + 2 + "/pageSize/" + 10,
-                        PageResults.class);
+                        PageResults.class);*/
+
+        String contentAsString = mockMvc
+                .perform(
+                        get("/Log/getLogByPage/pageNumber/" + 2 + "/pageSize/" + 10)
+                )
+                .andDo(print())
+                .andExpect(status().is(200))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        System.out.println(contentAsString);
+        //手动将string转为json
+        ObjectMapper objectMapper = new ObjectMapper();
+        PageResults pageResults = objectMapper.readValue(contentAsString, PageResults.class);
+
+
+        /*Map map = objectMapper.readValue(contentAsString, Map.class);
+        System.out.println(map);
+        int previousPage = Integer.parseInt(map.get("previousPage").toString());
+        int currentPage = Integer.parseInt(map.get("currentPage").toString());
+        int nextPage = Integer.parseInt(map.get("nextPage").toString());
+        int pageSize = Integer.parseInt(map.get("pageSize").toString());
+        int totalCount = Integer.parseInt(map.get("totalCount").toString());
+        int pageCount = Integer.parseInt(map.get("pageCount").toString());
+        assertTrue(previousPage <= currentPage);
+        assertTrue(currentPage <= nextPage);
+        assertTrue(pageSize * pageCount >= totalCount);
+
+        int i = totalCount % pageSize;
+        if (i == 0) {
+            assertEquals(totalCount / pageSize, pageCount);
+        } else {
+            assertEquals(totalCount / pageSize + 1, pageCount);
+        }*/
+
 
         int previousPage = pageResults.getPreviousPage();
         int currentPage = pageResults.getCurrentPage();
@@ -90,44 +125,5 @@ public class LogControllerTest extends BaseTest {
         } else {
             assertEquals(totalCount / pageSize + 1, pageCount);
         }
-
-
-
-       /* Object forObject = restTemplate.getForObject(
-                uriHead+"/Log/getLogByPage/pageNumber/" + 2 + "/pageSize/" + 10
-                , Object.class
-        );
-        System.out.println(forObject);*/
-
-        /*String contentAsString = mockMvc
-                .perform(
-                        get("/Log/getLogByPage/pageNumber/" + 2 + "/pageSize/" + 10)
-                )
-                .andDo(print())
-                .andExpect(status().is(200))
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-        System.out.println(contentAsString);
-        //将string转为json
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map map = objectMapper.readValue(contentAsString, Map.class);
-        System.out.println(map);
-        int previousPage = Integer.parseInt(map.get("previousPage").toString());
-        int currentPage = Integer.parseInt(map.get("currentPage").toString());
-        int nextPage = Integer.parseInt(map.get("nextPage").toString());
-        int pageSize = Integer.parseInt(map.get("pageSize").toString());
-        int totalCount = Integer.parseInt(map.get("totalCount").toString());
-        int pageCount = Integer.parseInt(map.get("pageCount").toString());
-        assertTrue(previousPage <= currentPage);
-        assertTrue(currentPage <= nextPage);
-        assertTrue(pageSize * pageCount >= totalCount);
-
-        int i = totalCount % pageSize;
-        if (i == 0) {
-            assertEquals(totalCount / pageSize, pageCount);
-        } else {
-            assertEquals(totalCount / pageSize + 1, pageCount);
-        }*/
     }
 }
